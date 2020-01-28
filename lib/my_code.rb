51d617350cc8ebcbb0050384d35989d0e4baade2
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-28 15:16:03 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,14 @@
-# My Code here....
+# My includeCodmape here....
+
+def map_to_negativize(source_array)
+ index = 0
+ array = []
+
+ while index < source_array do
+array[index] = -source_array[index]
+
+end
+
+return array
+
+end
